3b3412450e579112549789d207d29d2f6b97a6dc
Ryan Walsh <rcwalsh@gmail.com>
2021-04-14 12:10:50 -0400

    scaleCoordinates seems to calculate correctly, but soundSource.setPosition still doesn't sound like it's working

--- src/utils/spatial.js
+++ src/utils/spatial.js
@@ -1,19 +1,16 @@
-// Create an AudioContext
-let audioContext = new AudioContext();
-
-// Create a (first-order Ambisonic) Resonance Audio scene and pass it the AudioContext.
-// Initialize scene and create Source(s).
-let scene = new window.ResonanceAudio(audioContext, {
-	ambisonicOrder: 1,
-});
-// Connect the scene’s binaural output to stereo out.
-scene.output.connect(audioContext.destination);
+const soundSources = {};
+let isMouseDown = false;
+let left = 0;
+const width = 500;
+const colors = ['red', 'blue', 'green'];
+let currentDiv;
 // By default, room dimensions are undefined (0m x 0m x 0m). https://resonance-audio.github.io/resonance-audio/develop/web/getting-started
 const roomDimensions = {
   width: 25,
   height: 25,
   depth: 25,
 };
+
 // Room materials have different acoustic reflectivity.
 const roomMaterials = {
   // Room wall materials. https://resonance-audio.github.io/resonance-audio/develop/web/getting-started
@@ -24,14 +21,19 @@ const roomMaterials = {
   down: 'transparent', // Room floor
   up: 'transparent', // Room ceiling
 };
+
+// Create an AudioContext
+let audioContext = new AudioContext();
+
+// Create a (first-order Ambisonic) Resonance Audio scene and pass it the AudioContext.
+// Initialize scene and create Source(s).
+let scene = new window.ResonanceAudio(audioContext, {
+	ambisonicOrder: 1,
+});
+
 scene.setRoomProperties(roomDimensions, roomMaterials);
+// Connect the scene’s binaural output to stereo out.
 scene.output.connect(audioContext.destination);
-const soundSources = {};
-let isMouseDown = false;
-let left = 0;
-const width = 500;
-const colors = ['red', 'blue', 'green'];
-let currentDiv;
 
 function getXY (event, offsetJson) {
 	const mousePosition = {
@@ -52,13 +54,30 @@ function setSoundCoordinates(participantUserId, point) {
 	console.log(soundSource, x, y, z);
 }
 
-function scaleCoordinates(point, roomDimensions) { // TODO
-	const { x, y, z } = point;
-	return {
-		x: x / 100,
-		y: y / 100,
-		z
-	}
+function scaleCoordinates(currentDiv, roomDimensions, container) {
+    const videoCenter = getCenterCoordinates(currentDiv);
+    const containerCenter = getCenterCoordinates(container);
+    const x = (videoCenter.x - containerCenter.x) / containerCenter.x * roomDimensions.width;
+    const y = (videoCenter.y - containerCenter.y) / containerCenter.y * roomDimensions.height * -1;
+    const result = {
+        x,
+        y,
+        z: 1
+    };
+    console.log({ videoCenter, containerCenter, result });
+    return result;
+}
+
+/**
+ * @see https://stackoverflow.com/questions/8027875/how-to-find-the-elements-x-center-coordinates-and-related-window-offset/46921780#comment95027215_46921780
+ * 
+ * @param {element} element 
+ * @returns {object}
+ */
+function getCenterCoordinates (element) {
+    const x = element.offsetLeft + element.offsetWidth / 2;
+    const y = element.offsetTop + element.offsetHeight / 2;
+    return { x, y };
 }
 
 function getRestrictedPosition(div, event) {
@@ -120,8 +139,9 @@ document.addEventListener('mousemove', function (event) {
 		const { x, y } = getXY(event, currentDiv.getAttribute('data-offset'));
 		currentDiv.style.left = x + 'px';
 		currentDiv.style.top = y + 'px';
-		const participantUserId = currentDiv.getAttribute('data-participantUserId');
-		const point = scaleCoordinates({ x, y, z: 1 }, roomDimensions);
+        const participantUserId = currentDiv.getAttribute('data-participantUserId');
+        const container = document.getElementById('main');
+		const point = scaleCoordinates(currentDiv, roomDimensions, container);
 		setSoundCoordinates(participantUserId, point);
     }
 }, true);
